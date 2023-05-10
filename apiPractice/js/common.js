ce6fda8bfa24d7e2b16d26c6cebd56bb9909e728
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-10 14:24:29 +0900

    채ㅡㅡㅑㅅ

--- apiPractice/js/common.js
+++ apiPractice/js/common.js
@@ -1,23 +1,31 @@
 function runAPI() {
-  const url = document.getElementById("urlInputName").value;
-  if (!url) {
+const url = document.getElementById("urlInputName").value;
+    if (!url) {
     console.log("url을 입력해주세요.");
     return;
-  }
-  fetch(url)
+    }
+fetch(url)
     .then(res => res.json())
     .then(data => {
-      makeList(data);
-      document.getElementById("imageContainer").style.display = "block"; // 이미지 컨테이너 보이기
+    makeList(data);
+    document.getElementById("imageContainer").style.display = "block"; // 이미지 컨테이너 보이기
     })
     .catch(console.log);
-    
-  const imageContainer = document.getElementById("imageContainer");
-  if (imageContainer.innerHTML) { // 이미지가 존재할 경우
-    imageContainer.innerHTML = ""; // 이미지 삭제
-    imageContainer.style.display = "none"; // 이미지 컨테이너 숨기기
-  }
+    const imageContainer = document.getElementById("imageContainer");
+    if (imageContainer.innerHTML) { // 이미지가 존재할 경우
+      imageContainer.innerHTML = ""; // 이미지 삭제
+      imageContainer.style.display = "none"; // 이미지 컨테이너 숨기기
+    }
 }
+
+function resetAPI() {
+    const imageContainer = document.getElementById("imageContainer");
+    if (imageContainer.innerHTML) { // 이미지가 존재할 경우
+      imageContainer.innerHTML = ""; // 이미지 삭제
+      imageContainer.style.display = "none"; // 이미지 컨테이너 숨기기
+    }
+}
+
 
 function makeList(data) {
     const imageContainer = document.getElementById("imageContainer");
