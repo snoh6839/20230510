ca1df9093a60cadc6b74e6523bb172ed00351ef7
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-10 14:07:44 +0900

    Update common.js

--- apiPractice/js/common.js
+++ apiPractice/js/common.js
@@ -1,14 +1,24 @@
 function runAPI() {
-    const url = document.getElementById("urlInputName").value;
-    if (!url) {
-        console.log("url을 입력해주세요.");
-        return;
-    }
-    fetch(url)
-        .then(res => res.json())
-        .then(data => makeList(data))
-        .catch(console.log);
+  const url = document.getElementById("urlInputName").value;
+  if (!url) {
+    console.log("url을 입력해주세요.");
+    return;
+  }
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      makeList(data);
+      document.getElementById("imageContainer").style.display = "block"; // 이미지 컨테이너 보이기
+    })
+    .catch(console.log);
+    
+  const imageContainer = document.getElementById("imageContainer");
+  if (imageContainer.innerHTML) { // 이미지가 존재할 경우
+    imageContainer.innerHTML = ""; // 이미지 삭제
+    imageContainer.style.display = "none"; // 이미지 컨테이너 숨기기
+  }
 }
+
 function makeList(data) {
     const imageContainer = document.getElementById("imageContainer");
     imageContainer.innerHTML = ""; // 이미지 컨테이너 초기화
