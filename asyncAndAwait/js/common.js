7a5b6d3c1e1c86bde15d702628d21f4538e0b8ea
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-10 11:13:53 +0900

    async await

--- asyncAndAwait/js/common.js
+++ asyncAndAwait/js/common.js
@@ -1,29 +1,48 @@
-function delay() {
-    const delayTime = Date.now() +2000;
-    while (Date.now() < delayTime) { }
-    console.log('B');
-}
-console.log('A');
-delay();
-console.log('C');
+// async function delay() {
+//     const delayTime = Date.now() +2000;
+//     while (Date.now() < delayTime) {}
+//     return 'B';
+// }
 
+// console.log('A');
+// delay().then(b => console.log(b));
+// console.log('C');
 
-function delay2() {
-    return new Promise(
-        // function (resolve) {
-        //     const dealyTime = Date.now() + 2000;
-        //     while (Date.now() < delayTime) { }
-        //     resolve('B');
-        // }
-        (resolve) => {
-            const delayTime = Date.now() + 2000;
-            while (Date.now() < delayTime) { }
-            resolve('B');
-        }
-        );
-    }
+// function myDelay(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-console.log('A');
-delay2()
-.then( b => console.log(b));
-console.log('C');
+// function getA() {
+//     myDelay(1000);
+//     return 'A';
+// }
+
+// function getB() {
+//     myDelay(2000);
+//     return 'B';
+// }
+
+// console.log(getA() + getB())
+
+// // function getAll() {
+// //     getA()
+// //     .then(strA => {
+// //         return getB()
+// //             .then(strB => console.log(`${strA} : ${strB}`))
+// //     })
+// // }
+
+// async function getAllAsync() {
+//     const strA = await getA();
+//     const strB = await getB();
+//     console.log(`${strA} : ${strB}`);
+// }
+
+// // getAllAsync();
+
+// async function getAllAsyncPromis() {
+//     Promise.all([getA(), getB()])
+//     .then(all => all.join(' : '))
+// }
+
+// getAllAsyncPromis();
